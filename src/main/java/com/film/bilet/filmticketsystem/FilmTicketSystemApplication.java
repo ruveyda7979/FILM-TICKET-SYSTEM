bae505dabf9233f3e816e4b958d16ea1084d1164
ruveyda7979 <rvydkocaker@gmail.com>
2025-04-24 22:58:18 +0300

    İlk commit

---
+++ src/main/java/com/film/bilet/filmticketsystem/FilmTicketSystemApplication.java
@@ -0,0 +1,13 @@
+package com.film.bilet.filmticketsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmTicketSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmTicketSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/film/bilet/filmticketsystem/FilmTicketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.film.bilet.filmticketsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmTicketSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
